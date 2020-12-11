938dd69b0e81cf39bc5602b3698a5e1f797ed2d5
Josh <joshualepinski@gmail.com>
2020-12-10 22:07:14 -0700

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 import setuptools
 
 setup(name='pyconvcli',
-      version='0.0.1',
+      version='0.0.2',
       description='A convention based CLI framework for python',
       author='Joshua Lepinski',
       author_email='joshualepinski@gmail.com',
