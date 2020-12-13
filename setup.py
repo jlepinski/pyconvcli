cf04d0f0729b93d9baf6f67516f9868f34f287c7
Josh <joshualepinski@gmail.com>
2020-12-13 15:03:28 -0700

    added long description in the form of the current readme

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 import setuptools
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(name='pyconvcli',
       version='0.0.2',
       description='A convention based CLI framework for python',
@@ -12,6 +17,8 @@ setuptools.setup(name='pyconvcli',
       ],
       packages=setuptools.find_packages(),
       include_package_data=True,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       install_requires=[
           'pydash==4.9.0',
           'stringcase==1.2.0'
