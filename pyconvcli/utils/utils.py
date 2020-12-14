79049cce25e85a349cd095e3c3f68f6c5291d320
Josh <joshualepinski@gmail.com>
2020-12-13 18:57:09 -0700

    removed files that are no longer used

--- pyconvcli/utils/utils.py
+++
@@ -1,8 +0,0 @@
-import pkg_resources
-class Utility_CLI():
-    _cli_path=[]
-    def version(self):
-        print(pkg_resources.get_distribution("pyconvcli").version)
-    def smile(self):
-        print(":)")
-
