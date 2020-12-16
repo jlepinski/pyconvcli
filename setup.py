514e271b0ee8625f4d97b1ec091b387e6c60a349
jlepinski <jlepinski@users.noreply.github.com>
2020-12-15 23:30:53 -0700

    Release/0.0.4 (#4)
    * will it run on once or do I need it on push:
    
    * will it rerun the tests on new commits to the pr
    
    * removed superfluous print statments and updated the test to combine the cd command with the setup for the sub cli
    
    * need to remember that the python install on windows doesn't have 3 referencing it
    
    * updated version and readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(name='pyconvcli',
-      version='0.0.3',
+      version='0.0.4',
       description='A convention based CLI framework for python',
       author='Joshua Lepinski',
       author_email='joshualepinski@gmail.com',
