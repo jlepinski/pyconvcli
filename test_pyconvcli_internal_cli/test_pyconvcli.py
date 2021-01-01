b8148bc58acf020be97d3ef3435b2dfcb0979d19
Josh Lepinski <joshlepinski@Joshs-Mac-mini.local>
2020-12-31 18:15:15 -0700

    missed another rename

--- test_pyconvcli_internal_cli/test_pyconvcli.py
+++ test_pyconvcli_internal_cli/test_pyconvcli.py
@@ -9,9 +9,9 @@ from io import StringIO
 class TestPyConvCli(unittest.TestCase):
 
     def test_update_parser_for_functions(self):
-        sys.argv = ["test", "here", 'custom', 'route']
+        sys.argv = ['test_pyconvcli_internal_cli', "here", 'custom', 'route']
 
-        cli = PyConvCli('test', os.path.dirname(os.path.realpath(__file__)),'pyconvcli-test')
+        cli = PyConvCli('test_pyconvcli_internal_cli', os.path.dirname(os.path.realpath(__file__)),'pyconvcli-test')
         args, parsers = cli.parse_args()
         self.assertEqual(len(parsers['test.here.custom.route']['callables']), 2)
 
