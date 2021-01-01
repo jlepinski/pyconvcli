282acfcd21a0f818ec109c5c874ca461be19d929
Josh Lepinski <joshlepinski@Joshs-Mac-mini.local>
2020-12-31 18:56:58 -0700

    updated version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 
 setuptools.setup(name='pyconvcli',
-      version='0.0.4',
+      version='0.0.5',
       description='A convention based CLI framework for python',
       author='Joshua Lepinski',
       author_email='joshualepinski@gmail.com',
